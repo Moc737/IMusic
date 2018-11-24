08b62288ce3c943f3e6c47eccb39dee69b02cb63
Marat <0775448400@mail.ru>
2018-11-25 00:03:59 +0100

    это мои 1 комит

---
+++ js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+console.log(4/0);
+console.log('string'*7)
+
+let something;
+console.log(something)
+
+let persone = {
+    name: "Marat",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['music.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[2])
+
+//alert("hellow world!");
+
+/*let answer = confirm("Are you here?");
+
+console.log(answer)*/
+
+let answer = prompt("есть ли вам 18?", "да");
+
+console.log(typeof(answer));
